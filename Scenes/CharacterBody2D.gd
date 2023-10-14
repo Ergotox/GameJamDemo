083c1a437db965f7af6f10d463ef060f65ca302f
Diego Santivañez <Diegojsf17@gmail.com>
2023-10-14 13:18:20 -0500

    feat: animation world exploration added

--- Scenes/CharacterBody2D.gd
+++ Scenes/CharacterBody2D.gd
@@ -11,14 +11,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	var direction = Input.get_axis("up", "down")
 	if direction > 0:
-		position.y = direction + 30
-		$AnimationPlayer.play("top")
-		position.y = direction + 30
+		position.y = direction * 2
 		$AnimationPlayer.play("RightSide")
 	elif direction < 0:
-		position.y = direction - 30
-		$AnimationPlayer.play("bot")
-		position.y = direction - 30
+		position.y = direction * -2
 		$AnimationPlayer.play("RightSide")
 
 	move_and_slide()
