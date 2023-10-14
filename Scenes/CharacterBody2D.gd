61567c38c95173683163c03f7c562be1ff33ecd4
Diego Santivañez <Diegojsf17@gmail.com>
2023-10-14 16:56:25 -0500

    feat: nothing important

--- Scenes/CharacterBody2D.gd
+++ Scenes/CharacterBody2D.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
+
 func _physics_process(delta):
 	if Input.is_action_pressed("up"):
-		position.y = position.y - 20
-		$AnimationPlayer.play("top")
+		if position.y < 448:
+			position.y = 384
+			$AnimationPlayer.play("top")	
 	elif Input.is_action_pressed("down"):
 		position.y = position.y + 20
 		$AnimationPlayer.play("RightSide")
 	move_and_slide()
+
