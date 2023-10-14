af395be3605f1f6a642daa55768ce87de05e2dc3
Diego Santivañez <Diegojsf17@gmail.com>
2023-10-14 16:14:06 -0500

    feat: move

--- Scenes/CharacterBody2D.gd
+++ Scenes/CharacterBody2D.gd
@@ -1,18 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 func _physics_process(delta):
-	var direction = Input.get_axis("up", "down")
-	if direction > 0:
-		position.y = direction * 2
+	if Input.is_action_pressed("up"):
+		position.y = position.y - 20
+		$AnimationPlayer.play("top")
+	elif Input.is_action_pressed("down"):
+		position.y = position.y + 20
 		$AnimationPlayer.play("RightSide")
-	elif direction < 0:
-		position.y = direction * -2
-		$AnimationPlayer.play("RightSide")
-
 	move_and_slide()
